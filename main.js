26701e13c22df4ab2c0dd6d495591f31b9f59337
Prado71 <u201711121@upc.edu.pe>
2022-04-20 03:25:59 -0500

    commit 01
    landing page

---
+++ main.js
@@ -0,0 +1,23 @@
+const button1 =document.getElementById('main1');
+
+button1.addEventListener('click',function handleClick(){
+    document.getElementById('check').checked=false;
+});
+
+const button2 =document.getElementById('main2');
+
+button2.addEventListener('click',function handleClick(){
+    document.getElementById('check').checked=false;
+});
+
+const button3 =document.getElementById('main3');
+
+button3.addEventListener('click',function handleClick(){
+    document.getElementById('check').checked=false;
+});
+
+const button4 =document.getElementById('main4');
+
+button4.addEventListener('click',function handleClick(){
+    document.getElementById('check').checked=false;
+});
